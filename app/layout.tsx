b5d7fc88f6f6b5a9bde1cf7ebfde769fa77ff8f2
JerwellSavella <jerwellsavella@gmail.com>
2024-07-07 01:45:03 +0800

    preview banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "Erick & Sugar",
+export const metadata = {
+  title: {
+    default: "Erick & Sugar",
+    template: "%s | Erick & Sugar",
+  }, 
   description: "We invite you to our special day on September 7, 2024",
+  metadataBase: new URL('https://ericksugar.vercel.app/'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'de-DE': '/de-DE',
+    },
+  },
+  openGraph: {
+    images: '/opengraph-image.png',
+  },
 };
 
 export default function RootLayout({
